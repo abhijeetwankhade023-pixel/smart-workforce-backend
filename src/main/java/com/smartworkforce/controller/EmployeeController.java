9c4af26daccf8e19fe45c7e5c5c7447bc42b6807
Abhijeet Wankhade <abhijeetwankhade023@gmail.com>
2026-09-21 16:58:09 +0530

    Day 2 - Complete Employee CRUD APIs

--- src/main/java/com/smartworkforce/controller/EmployeeController.java
+++ src/main/java/com/smartworkforce/controller/EmployeeController.java
@@ -6,10 +6,9 @@ import com.smartworkforce.service.EmployeeService;
 import jakarta.annotation.PostConstruct;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/employees")
@@ -25,5 +24,44 @@ public class EmployeeController {
     public ResponseEntity<Employee> saveEmployee( @RequestBody Employee employee) {
        Employee savedEmployee = employeeService.saveEmployee(employee);
        return ResponseEntity.ok().body(savedEmployee);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Employee>> getAllEmployees() {
+        List<Employee> employees = employeeService.getAllEmployees();
+        return ResponseEntity.ok().body(employees);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+
+        Employee employee = employeeService.getEmployeeById(id);
+
+        if(employee == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().body(employee);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Employee> updateEmployee(
+            @PathVariable Long id , @RequestBody Employee employee) {
+        Employee updatedEmployee = employeeService.updateEmployee(id, employee);
+
+        if(updatedEmployee == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().body(updatedEmployee);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable Long id) {
+
+        boolean deleted = employeeService.deleteEmployee(id);
+        if(!deleted) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().body("Employee deleted Successfully");
     }
 }
--- src/main/java/com/smartworkforce/service/EmployeeService.java
+++ src/main/java/com/smartworkforce/service/EmployeeService.java
@@ -5,6 +5,8 @@ import com.smartworkforce.entity.Employee;
 import com.smartworkforce.repository.EmployeeRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class EmployeeService {
 
@@ -17,5 +19,36 @@ public class EmployeeService {
     public Employee saveEmployee(Employee employee) {
 
         return employeeRepository.save(employee);
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee getEmployeeById(Long id) {
+        return employeeRepository.findById(id).orElse(null);
+    }
+
+    public Employee  updateEmployee( Long id ,Employee employee) {
+
+        Employee existingEmployee = employeeRepository.findById(id).orElse(null);
+        if (existingEmployee == null) {
+            return null;
+        }
+        existingEmployee.setName(employee.getName());
+        existingEmployee.setEmail(employee.getEmail());
+        existingEmployee.setDepartment(employee.getDepartment());
+        existingEmployee.setSalary(employee.getSalary());
+         return employeeRepository.save(existingEmployee);
+
+    }
+
+    public boolean deleteEmployee(Long id) {
+        Employee employee = employeeRepository.findById(id).orElse(null);
+        if (employee == null) {
+            return false;
+        }
+        employeeRepository.delete(employee);
+        return true;
     }
 }
